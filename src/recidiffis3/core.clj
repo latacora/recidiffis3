634040402de224fb93cb399027a6ad564fa80746
Laurens Van Houtven (lvh) <lvh@latacora.com>
2019-03-21 22:41:09 -0500

    More mellowing

--- src/recidiffis3/core.clj
+++ src/recidiffis3/core.clj
@@ -50,8 +50,6 @@
   "Given a Lambda S3 event, finds all object puts in the event and sends diffs off
   somewhere."
   [this in-stream out-stream context]
-  (-> context .getLogger (.log "log statement via lambda context logger"))
-  (log/info "recidiffis3 handleRequest starting...")
   (let [results (for [{s3-event :s3 region :awsRegion} (-> in-stream parse-json :Records)
                       :when s3-event
                       :let [{:keys [bucket object]} s3-event
